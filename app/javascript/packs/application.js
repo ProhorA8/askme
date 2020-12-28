a1de2cd80b64707e9357e08738cf0fd179a3013f
ProhorA8 <Procustom4@gmail.com>
2020-12-28 21:03:09 +0300

    add style avatar user, add bootstrap, new tagcloud

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,17 +1,10 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-require ("jquery")
+import './jquery.tagcloud'
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-
-Rails.start()
-// Turbolinks.start()
-ActiveStorage.start()
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
 
 document.addEventListener("turbolinks:load", function () {
     $(function () {
@@ -20,4 +13,10 @@ document.addEventListener("turbolinks:load", function () {
             return false;
         });
     });
-})
+
+    // Облако тэгов
+    $('#tags a').tagcloud({
+        size: { start: 12, end: 30, unit: 'pt' },
+        color: { start: '#aaa', end: '#37474F' }
+    });
+});
