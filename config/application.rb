09ceeff3b6620e5fb722035d5e1439a0dc1b0abf
ProhorA8 <Procustom4@gmail.com>
2021-01-03 00:25:00 +0300

    add questions controller

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,8 @@ module Askme
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.time_zone = 'Minsk'
     config.i18n.default_locale = :ru
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, except: [:destroy]
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: %i[new create destroy]
   resources :questions, except: %i[show new index]
 
   get 'sign_up' => 'users#new'
