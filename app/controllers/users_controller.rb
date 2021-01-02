bb1b73f96f14ecf7b3bd216cc2c272ad225baf87
ProhorA8 <Procustom4@gmail.com>
2021-01-03 00:56:56 +0300

    add authorize after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
     else
       render :new
