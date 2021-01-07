7b6c34924f24d153c8755335b856f138dba7b835
ProhorA8 <Procustom4@gmail.com>
2021-01-07 15:45:47 +0300

    new db tags, tag_questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_164707) do
+ActiveRecord::Schema.define(version: 2021_01_07_123440) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
